2da6673a87739c8e1ee218d07d30de91bd5ecc3c
Miroslaw Gabrus <poczta@mirekgab.pl>
2022-12-15 17:15:59 +0100

    add CrossOrigin annotation

--- src/main/java/pl/byczazagroda/trackexpensesappbackend/controller/WalletController.java
+++ src/main/java/pl/byczazagroda/trackexpensesappbackend/controller/WalletController.java
@@ -28,6 +28,7 @@ import java.util.List;
 @RequiredArgsConstructor
 @RestController
 @RequestMapping("api/wallet")
+@CrossOrigin
 public class WalletController {
 
     private final WalletService walletService;
